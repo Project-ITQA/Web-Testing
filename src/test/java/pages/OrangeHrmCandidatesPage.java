810e127ba418ba4040fcb038ade2df457b3806bf
Geethaka Pitigala <66256789+geethaka-pitigala@users.noreply.github.com>
2024-12-25 18:58:52 +0530

    Delete Candidate Scenario Test implementation - StepDefinition file - Additional methods for CandidatesPageStepLibrary - Additional methods for OrangeHrmCandidatesPageCandidatesPageObject

--- src/test/java/pages/OrangeHrmCandidatesPage.java
+++ src/test/java/pages/OrangeHrmCandidatesPage.java
@@ -1,13 +1,61 @@
 package pages;
 
 import net.serenitybdd.annotations.DefaultUrl;
+import net.serenitybdd.core.annotations.findby.By;
 import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.core.pages.WebElementFacade;
 import net.serenitybdd.screenplay.ui.Button;
+import org.junit.Assert;
+import utils.TestUtils;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 @DefaultUrl("https://opensource-demo.orangehrmlive.com/web/index.php/recruitment/viewCandidates")
 public class OrangeHrmCandidatesPage extends PageObject {
 
+    private WebElementFacade getCandidateRow(String firstName, String lastName) {
+        for (WebElementFacade row : findAll(By.cssSelector(".oxd-table-row"))) {
+            if(row.getText().toLowerCase().contains(firstName.toLowerCase()) && row.getText().toLowerCase().contains(lastName.toLowerCase())) {
+                return row;
+            }
+        }
+        return null;
+    }
+
+    private WebElementFacade getCandidateDeleteButton(String firstName, String lastName) {
+        WebElementFacade row = getCandidateRow(firstName, lastName);
+        if(row != null) {
+            return row.find(By.cssSelector("button i.bi-trash"));
+        }
+        return null;
+    }
+
     public void clickAddCandidateButton(){
         $(Button.withText("Add")).click();
+    }
+
+    public void verifyCandidateRow(String firstName, String lastName) {
+        TestUtils.addDelay(3000);
+        WebElementFacade row = getCandidateRow(firstName, lastName);
+        Assert.assertNotNull(row);
+    }
+
+    public void clickDeleteButton(String firstName, String lastName) {
+        WebElementFacade deleteButton = getCandidateDeleteButton(firstName, lastName);
+        if(deleteButton != null) {
+            deleteButton.click();
+        }else{
+            Assert.fail("Could not find delete button");
+        }
+    }
+
+    public void clickConfirmDeleteButton() {
+        TestUtils.addDelay(3000);
+        withTimeoutOf(5, SECONDS).waitForPresenceOf(By.cssSelector(".oxd-button--label-danger")).click();
+    }
+
+    public void verifyNotCandidateRow(String firstName, String lastName) {
+        WebElementFacade row = getCandidateRow(firstName, lastName);
+        Assert.assertNull(row);
     }
 }
--- src/test/resources/features/ManageCandidates.feature
+++ src/test/resources/features/ManageCandidates.feature
@@ -1,10 +1,12 @@
 Feature: Manage Candidates
 
-  Scenario: Add Candidate
+  Background:
     Given user is on home page
     When user enters "Admin" as username
     And user enters "admin123" as password
     Then user should be able to login
+
+  Scenario: Add Candidate
     Given User is on the Recruitment Candidates page
     And User clicks on the Add button
     And User is navigated to the Add Candidate page
@@ -12,3 +14,17 @@ Feature: Manage Candidates
     When User clicks the submit button
     Then User is directed to the view candidate page
     Then User can see candidate "Saman" "Kumara"
+
+  Scenario: Delete Candidate
+    Given User is on the Recruitment Candidates page
+    And User clicks on the Add button
+    And User is navigated to the Add Candidate page
+    And User fills the first name "Nuwan" and last name "Kumara" and email as "nuwan@gmail.com"
+    When User clicks the submit button
+    Then User is directed to the view candidate page
+    Then User can see candidate "Nuwan" "Kumara"
+    Given User is on the Recruitment Candidates page
+    Given User can see "Nuwan" "Kumara" in Candidates table
+    When User clicks on the Delete button of "Nuwan" "Kumara"
+    When User clicks on the confirm button
+    Then User cannot see "Nuwan" "Kumara" in candidates table
