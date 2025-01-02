a512286026be309e7e965d9909ef20ddbf4b64ce
Chethiya Nuwan <115537042+ChethiyaNuwan@users.noreply.github.com>
2025-01-02 20:51:33 +0530

    added an add delay method with default delay

--- src/test/java/utils/TestUtils.java
+++ src/test/java/utils/TestUtils.java
@@ -1,9 +1,19 @@
 package utils;
 
 public class TestUtils {
+    private static final int defaultDelay = 3000;
+
     public static void addDelay(int milliseconds) {
         try {
             Thread.sleep(milliseconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void addDelay() {
+        try {
+            Thread.sleep(defaultDelay);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
