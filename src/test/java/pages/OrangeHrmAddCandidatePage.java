d4c58931a9fbdc9f7e8a662f411effd0bed0540e
Geethaka Pitigala <66256789+geethaka-pitigala@users.noreply.github.com>
2025-01-03 23:50:14 +0530

    modified according new incoming changes from main

---
+++ src/test/java/pages/OrangeHrmAddCandidatePage.java
@@ -0,0 +1,57 @@
+package pages;
+
+import net.serenitybdd.annotations.DefaultUrl;
+import net.serenitybdd.core.annotations.findby.FindBy;
+import net.serenitybdd.core.exceptions.NoSuchElementException;
+import net.serenitybdd.core.pages.WebElementFacade;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+
+@DefaultUrl("https://opensource-demo.orangehrmlive.com/web/index.php/recruitment/addCandidate")
+public class OrangeHrmAddCandidatePage extends PageObjectBase {
+    @FindBy(name="firstName")
+    private WebElementFacade firstNameField;
+
+    @FindBy(name="lastName")
+    private WebElementFacade lastNameField;
+
+    @FindBy(xpath = "//label[text()='Email']/ancestor::div[contains(@class, 'oxd-input-group')]//input")
+    private WebElementFacade emailField;
+
+    @FindBy(css = "button[type='submit']")
+    private WebElementFacade submitButton;
+
+    public void fillFirstName(String firstName) {
+        firstNameField.sendKeys(firstName);
+    }
+
+    public void fillLastName(String lastName) {
+        lastNameField.sendKeys(lastName);
+    }
+
+    public void fillEmail(String email) {
+        emailField.sendKeys(email);
+    }
+
+    public void verifyCurrentPage()
+    {
+        String currentURL = (getDriver().getCurrentUrl());
+        System.out.println(currentURL);
+        assert currentURL != null;
+        Assert.assertTrue(currentURL.contains("recruitment/addCandidate"));
+    }
+
+    public void clickSubmitButton() {
+        submitButton.click();
+    }
+
+    public void verifyEmailRequiredMessage(){
+        try {
+            WebElementFacade requiredMessage = find(By.xpath("//label[text()='Email']/ancestor::div[contains(@class, 'oxd-input-group')]/descendant::span[text()='Required']"));
+            requiredMessage.waitUntilVisible();
+            Assert.assertTrue(requiredMessage.isDisplayed());
+        } catch (NoSuchElementException e) {
+            Assert.fail("Email required message is not shown");
+        }
+    }
+}
---
+++ src/test/java/pages/OrangeHrmCandidatesPage.java
@@ -0,0 +1,99 @@
+package pages;
+
+import net.serenitybdd.annotations.DefaultUrl;
+import net.serenitybdd.core.annotations.findby.By;
+import net.serenitybdd.core.pages.WebElementFacade;
+import net.serenitybdd.screenplay.ui.Button;
+import org.junit.Assert;
+import utils.TestUtils;
+import java.util.List;
+
+@DefaultUrl("https://opensource-demo.orangehrmlive.com/web/index.php/recruitment/viewCandidates")
+public class OrangeHrmCandidatesPage extends PageObjectBase {
+
+    private WebElementFacade getCandidateRow(String firstName, String lastName) {
+        for (WebElementFacade row : findAll(By.cssSelector(".oxd-table-row"))) {
+            if(row.getText().toLowerCase().contains(firstName.toLowerCase()) && row.getText().toLowerCase().contains(lastName.toLowerCase())) {
+                return row;
+            }
+        }
+        return null;
+    }
+
+    private WebElementFacade getCandidateDeleteButton(String firstName, String lastName) {
+        WebElementFacade row = getCandidateRow(firstName, lastName);
+        if(row != null) {
+            return row.find(By.cssSelector("button i.bi-trash"));
+        }
+        return null;
+    }
+
+    public void clickAddCandidateButton(){
+        $(Button.withText("Add")).click();
+    }
+
+    public void verifyCandidateRow(String firstName, String lastName) {
+        WebElementFacade row = getCandidateRow(firstName, lastName);
+        Assert.assertNotNull(row);
+    }
+
+    public void clickDeleteButton(String firstName, String lastName) {
+        WebElementFacade deleteButton = getCandidateDeleteButton(firstName, lastName);
+        if(deleteButton != null) {
+            deleteButton.click();
+        }else{
+            Assert.fail("Could not find delete button");
+        }
+    }
+
+    public void clickConfirmDeleteButton() {
+        TestUtils.addDelay(3000);
+        WebElementFacade confirmDeleteButton = find(By.cssSelector(".oxd-button--label-danger"));
+        confirmDeleteButton.waitUntilVisible();
+        confirmDeleteButton.click();
+    }
+
+    public void verifyNotCandidateRow(String firstName, String lastName) {
+        WebElementFacade row = getCandidateRow(firstName, lastName);
+        Assert.assertNull(row);
+    }
+
+    public void typeIntoSearchBox(String text) {
+        WebElementFacade searchBox = find(By.xpath("//div[contains(@class,'oxd-input-group') and .//label[text()='Candidate Name']]//input"));
+        searchBox.waitUntilVisible();
+        searchBox.sendKeys(text);
+    }
+
+    public void verifySearchDropDownItemExists(String firstName, String lastName) {
+        try{
+            WebElementFacade dropDownItem = find(By.xpath("//div[contains(@class, 'oxd-autocomplete-option') and contains(text(), '"+ firstName + "') and contains(text(), '" + lastName + "')]"));
+            dropDownItem.waitUntilVisible();
+            Assert.assertNotNull(dropDownItem);
+        } catch (Exception e) {
+            Assert.fail("Could not find search drop down item");
+        }
+    }
+
+    public void clickDropDownItem(String firstName, String lastName) {
+        try{
+            WebElementFacade dropDownItem = find(By.xpath("//div[contains(@class, 'oxd-autocomplete-option') and contains(text(), '" + firstName + "') and contains(text(), '" + lastName + "')]"));
+            dropDownItem.waitUntilVisible();
+            dropDownItem.click();
+        }catch (Exception e) {
+            Assert.fail("Could not find drop down item");
+        }
+    }
+
+    public void clickSearchButton(){
+        WebElementFacade searchButton = find(By.xpath("//button[contains(text(), 'Search')]"));
+        searchButton.waitUntilVisible();
+        searchButton.click();
+    }
+
+    public void verifyOnlyOneRowExists(String firstName, String lastName) {
+        List<WebElementFacade> rows = findAll(By.cssSelector(".oxd-table-row"));
+        Assert.assertEquals("Expected exactly one visible row, but found " + rows.size(), 1, rows.size());
+        String rowText = rows.getFirst().getText().toLowerCase();
+        Assert.assertTrue(rowText.contains(firstName.toLowerCase()) && rowText.contains(lastName.toLowerCase()));
+    }
+}
